0c046d7b138256f9928ddca2867f25020fc89b92
Sarsande-Sunil <priyankaingle250@gmail.com>
2022-11-27 22:46:37 +0530

    added

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,19 +6,56 @@ const db = require("./db.json")
 
 //middleware
 const logger = (req, res, next) => {
-    console.log("Before", req.method,req.url);
+    console.log("Before", req.method, req.url);
     next();
-    console.log("After",req.method,req.url)
+    console.log("After", req.method, req.url)
+};
+
+const logger2 = (req, res, next) => {
+    console.log("Before2", req.method, req.url);
+    next();
+    console.log("After2", req.method, req.url);
+    
+};
+
+const auth = (req, res) => {
+    if (
+        req.body &&
+        req.body.username === "Priyanka"
+        && req.body.password === "1234") {
+        next();
+        
+    } else {
+        res.send("Not Authnticate")
+    }
+
+        
+    
+    
+    
 }
+    
+
 
 const app = express();
-app.use(logger);
+app.use(cors());
+app.use(auth)
+// app.use(logger);
+// app.use(logger2);
+
 
 //get
 
 app.get("/", (req, res) => {
-    console.log("Request",req.method,req.url)
+    console.log("Request", req.method, req.url);
+    res.send("Sucess");
 })
+
+//post
+app.post("/", (req, res) => {
+  console.log("Request", req.method, req.url);
+  res.send("Failed");
+});
 
 // app.get("/products", (req, res) => {
 //     res.send(db.products)
